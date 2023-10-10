8f77624103149cd5945d327306d3ed64d6232725
Nikita Shcherbakov <unshackled39@gmail.com>
2023-10-10 15:57:52 +0300

    fix (BackgroundVideo.tsx): hot fix

--- src/app/views/Header/BackgroundVideo.tsx
+++ src/app/views/Header/BackgroundVideo.tsx
@@ -44,6 +44,7 @@ const BackgroundVideo = ({ screenWidthMobile }: { screenWidthMobile: boolean }) 
           loop
           muted
           playing
+          playsInline
           url={screenWidthMobile ? mobileVideoBackground : videoSourcePc}
           width='100%'
           height='100%'
