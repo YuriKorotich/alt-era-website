38244a320fd669e16ea2162b8c88d330298b5689
Nikita Shcherbakov <unshackled39@gmail.com>
2023-10-19 13:04:04 +0300

    fix(robots, sitemap): fixes for errors with incorrect connection of files (robots, sitemap)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const path = require('path');
 
 const nextConfig = {
   output: 'export',
+  async rewrites() {
+    return [
+      {
+        source: '/robots.txt',
+        destination: '/api/robots.txt',
+      },
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap.xml',
+      },
+    ];
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
