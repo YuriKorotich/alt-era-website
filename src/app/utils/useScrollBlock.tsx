9217c7bd64edd23124f35c0d98c69930c1ee1846
Nikita Shcherbakov <unshackled39@gmail.com>
2023-10-19 15:05:47 +0300

    fix(ModalWindow.tsx): fixes height

--- src/app/utils/useScrollBlock.tsx
+++ src/app/utils/useScrollBlock.tsx
@@ -23,8 +23,8 @@ const useScrollBlock = () => {
      * 2. Fixes a bug in desktop Safari where `overflowY` does not prevent
      *    scroll if an `overflow-x` style is also applied to the body.
      */
-    html.style.position = 'relative'; /* [1] */
-    body.style.position = 'relative'; /* [1] */
+    html.style.position = 'fixed'; /* [1] */
+    body.style.position = 'fixed'; /* [1] */
     html.style.overflow = 'hidden'; /* [2] */
     body.style.overflow = 'hidden'; /* [2] */
     body.style.paddingRight = `${bodyPaddingRight + scrollBarWidth}px`;
